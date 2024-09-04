01b25275d5d348aa1ff3eae4f71d11913d854b82
Dhruv gupta <145158632+dhruv8815@users.noreply.github.com>
2024-09-05 01:19:03 +0530

    add reset button

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 
 function Counter() {
-    const [count, setCount] = useState(0)
+    const initialState = 0
+    const [count, setCount] = useState(initialState)
     const [second, setSecond] = useState(0)
 
     useEffect(() => {
@@ -24,6 +25,10 @@ function Counter() {
     const decrement5 = () => {
         setCount(prevCount => prevCount - 5)
     }
+
+    const reset = () => {
+        setCount(initialState)
+    }
     return (
         <div>
             <h1>Timer: {second}</h1>
@@ -32,6 +37,7 @@ function Counter() {
             <button onClick={decrement}>Decrement</button>
             <button onClick={increment5}>Incrementby5</button>
             <button onClick={decrement5}>Decrementby5</button>
+            <button onClick={reset}>Reset</button>
         </div>
     )
 }
