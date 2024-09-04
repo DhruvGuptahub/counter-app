fa16c40cf5d3da2877fe04ca84527a68ce5eab26
Dhruv gupta <145158632+dhruv8815@users.noreply.github.com>
2024-09-05 01:10:24 +0530

    first commit

---
+++ src/component/Counter.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState(0)
+    const [second, setSecond] = useState(0)
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setSecond((prevSecond) => prevSecond + 1)
+        }, 1000)
+
+        return () => clearInterval(intervalId)
+    }, [])
+
+    const increment = () => {
+        setCount(prevCount => prevCount + 1)
+    }
+    const increment5 = () => {
+        setCount(prevCount => prevCount + 5)
+    }
+    const decrement = () => {
+        setCount(prevCount => prevCount - 1)
+    }
+    const decrement5 = () => {
+        setCount(prevCount => prevCount - 5)
+    }
+    return (
+        <div>
+            <h1>Timer: {second}</h1>
+            <h1>Count: {count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+            <button onClick={increment5}>Incrementby5</button>
+            <button onClick={decrement5}>Decrementby5</button>
+        </div>
+    )
+}
+
+export default Counter
